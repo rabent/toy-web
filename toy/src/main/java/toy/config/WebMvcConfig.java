507229ac2f77e132c8072bd78e52c8dc5805c2e1
rabent <rabent0207@gmail.com>
2025-04-14 20:48:52 +0900

    css, js 깨짐, 이미지 엑박 문제 다시 해결

--- toy/src/main/java/toy/config/WebMvcConfig.java
+++ toy/src/main/java/toy/config/WebMvcConfig.java
@@ -1,16 +1,30 @@
 package toy.config;
 
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Configuration
 @Slf4j
 public class WebMvcConfig implements WebMvcConfigurer {
+    
+    @Value("${file.dir}")
+    private String fileDir;
+    
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**")
-                        .addResourceLocations("file:src/main/resources/static/");
+        // 정적 리소스 설정
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
+        
+        // 업로드된 파일 설정
+        registry.addResourceHandler("/files/**")
+                .addResourceLocations("file:" + fileDir)
+                .setCachePeriod(0);
+        
+        log.info("File upload path: {}", fileDir);
     }
 }
